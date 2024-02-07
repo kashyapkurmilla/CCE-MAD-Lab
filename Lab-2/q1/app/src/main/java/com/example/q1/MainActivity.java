a4a853f12b27de0a900843b8d5bd1ae4b3bcdfc1
kashyapkurmilla <kashyapkurmilla@gmail.com>
2024-02-07 16:39:32 +0530

    calc

--- Lab-2/q1/app/src/main/java/com/example/q1/MainActivity.java
+++ Lab-2/q1/app/src/main/java/com/example/q1/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.q1;
 
+import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() method called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() method called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() method called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() method called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() method called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() method called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() method called");
     }
 }
---
+++ Lab-2/q2/app/src/main/java/com/example/q2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.q2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
